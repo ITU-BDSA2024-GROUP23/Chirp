caf821218b737f03f5047e6a7accd56d76f18d0b
Philip Rosenhagen <phro@itu.dk>
2024-09-01 17:13:13 +0200

    Week1 complete
    Co-authored-by: Victor <vmem@itu.dk>
    Co-authored-by: Axel <axlu@itu.dk>
    Co-authored-by: Peter <pbjh@itu.dk>
    Co-authored-by: Kasper <kasjo@itu.dk>

---
+++ Chirp.CLI/Program.cs
@@ -0,0 +1,22 @@
+﻿var dbPath = "cheepDB.csv";
+
+if (args[0] == "cheep" && args.Length == 2) 
+{
+    var cheep = args[1];
+    var username = Environment.UserName;
+    var date = DateTime.Now;
+    
+    using var sw = new StreamWriter(dbPath, true);
+    var line = $"{date},{username},{cheep}";
+    sw.WriteLine(line);
+}
+
+if (args[0] == "read" && args.Length == 1)
+{
+    using var sr = new StreamReader(dbPath);
+    for (var line = sr.ReadLine(); line != null; line = sr.ReadLine()) 
+    {
+        var data = line.Split(',', 3);
+        Console.WriteLine($"{data[0]} @ {data[1]}: {data[2]}");
+    }
+}
