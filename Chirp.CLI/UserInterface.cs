b8268c99e4b12f9ef73f989b0193ba53655b71d5
vmem <vmem@itu.dk>
2024-09-10 18:24:52 +0200

    refactoring of Program.cs
    - changed User specific code to reside in a seperate class.
    
    Co-Authored-By: Kasper <57803719+Kanerix@users.noreply.github.com>
    Co-Authored-By: 21Philip <72072928+21Philip@users.noreply.github.com>

---
+++ Chirp.CLI/UserInterface.cs
@@ -0,0 +1,27 @@
+namespace Chirp.CLI;
+
+public static class UserInterface
+{
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps)
+    {
+        foreach(var cheep in cheeps)
+        {
+            Console.WriteLine(cheep.ToString());
+        }
+    }
+
+    public record Cheep(string Author, string Message, long Timestamp)
+    {
+        public override string ToString()
+        {
+            return Author + " @ " + UnixTimeStampToDateTime(Timestamp) + ": " + Message;
+        }
+
+        private static string UnixTimeStampToDateTime(long timestamp)
+        {
+            DateTime dateTime = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
+            return dateTime.ToString("dd-MM-yyyy HH:mm:ss");
+        }
+    }       
+}
