931ad6260ed98879e09c30f6e04e48049e93c3e8
vmem <vmem@itu.dk>
2024-09-19 17:58:22 +0200

    proper singleton implementation
    all db related tests doesnt work anymore :-1:

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -2,9 +2,8 @@ using SimpleDB;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-string filePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "..", "SimpleDB", "data", "cheepDB.csv")); // Surely there is a better way to do this?
 
-var db = CSVDatabase<Cheep>.GetInstance(filePath);
+var db = CSVDatabase<Cheep>.GetInstance();
 
 app.MapGet("/cheeps", () =>
 {
