df2d1ae404158c5dcf0493874101c87354d5a27e
vmem <vmem@itu.dk>
2024-10-01 18:49:49 +0200

    Revert "tests hotfix"
    This reverts commit c9e97973959e0c13765c3fa024d2e9b7fbfafd6d.

---
+++ test/Chirp.Razor.Tests/IntegrationTest.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Chirp.Razor.Tests;
+
+public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _fixture;
+    private readonly HttpClient _client;
+
+    public IntegrationTest(WebApplicationFactory<Program> fixture)
+    {
+        string dbPath = Path.Combine(Path.GetTempPath(), "integrationTests.db");
+        Environment.SetEnvironmentVariable("CHIRPDBPATH", dbPath);
+
+        _fixture = fixture;
+        _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions
+        {
+            AllowAutoRedirect = true,
+            HandleCookies = true,
+        });
+    }
+
+    [Fact]
+    public async void PublicTimelineAvailable()
+    {
+        var response = await _client.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains("Public Timeline", content);
+    }
+
+    [Theory]
+    [InlineData("Helge")]
+    public async void PrivateTimelineAvailable(string user)
+    {
+        var response = await _client.GetAsync($"/{user}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Chirp!", content);
+        Assert.Contains($"{user}'s Timeline", content);
+    }
+
+    [Theory]
+    [InlineData("Random user")]
+    public async void NoCheepsAvailable(string user)
+    {
+        var response = await _client.GetAsync($"/{user}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("There are no cheeps so far.", content);
+    }
+
+    [Fact]
+    public async void DifferentPages()
+    {
+        var response = await _client.GetAsync("/?page=0");
+        response.EnsureSuccessStatusCode();
+        var content1 = await response.Content.ReadAsStringAsync();
+
+        response = await _client.GetAsync("/?page=1");
+        response.EnsureSuccessStatusCode();
+        var content2 = await response.Content.ReadAsStringAsync();
+
+        Assert.NotEqual(content1, content2);
+    }
+
+}
