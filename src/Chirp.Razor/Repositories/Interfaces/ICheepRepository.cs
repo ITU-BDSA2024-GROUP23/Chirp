e3d24a1febcabbed727b3db2d3dbe53cb22365ee
vmem <vmem@itu.dk>
2024-10-03 17:07:18 +0200

    removal of chirp.db
    Co-Authored-By: 21Philip <72072928+21Philip@users.noreply.github.com>
    Co-Authored-By: Axel <118187716+Oksetorben@users.noreply.github.com>
    Co-Authored-By: pete537d <24959520+pete537d@users.noreply.github.com>

--- src/Chirp.Razor/Repositories/Interfaces/ICheepRepository.cs
+++ src/Chirp.Razor/Repositories/Interfaces/ICheepRepository.cs
@@ -2,7 +2,7 @@
 
 public interface ICheepRepository
 {
-    public Task<List<CheepDTO>> GetCheeps(int page = 0);
-    public Task<List<CheepDTO>> GetCheepsFromAuthor(string author, int page = 0);
+    public Task<List<CheepDTO>> GetCheeps(int page = 1);
+    public Task<List<CheepDTO>> GetCheepsFromAuthor(string author, int page = 1);
 }
 
