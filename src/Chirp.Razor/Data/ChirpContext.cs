6f78a62c6db81e2dd93b0a445db9d9de2c3c9ca9
vmem <vmem@itu.dk>
2024-10-03 14:25:58 +0200

    format

--- src/Chirp.Razor/Data/ChirpContext.cs
+++ src/Chirp.Razor/Data/ChirpContext.cs
@@ -5,9 +5,9 @@ public class ChirpContext : DbContext
     public DbSet<Cheep> Cheeps { get; set; }
     public DbSet<Author> Authors { get; set; }
 
-    public ChirpContext(DbContextOptions<ChirpContext> options ) : base(options)
+    public ChirpContext(DbContextOptions<ChirpContext> options) : base(options)
     {
-        
+
     }
 
 }
