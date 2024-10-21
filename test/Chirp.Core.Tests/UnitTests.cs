2fae8eaf0cc002f71c089acd1452aee262b877b1
vmem <vmem@itu.dk>
2024-10-21 15:59:29 +0200

    more core tests

--- test/Chirp.Core.Tests/UnitTests.cs
+++ test/Chirp.Core.Tests/UnitTests.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Core.Tests;
 
 public class UnitTests
@@ -23,5 +25,50 @@ public class UnitTests
 
         // Assert
         Assert.Equal("test", cheep.Text);
+    }
+
+    // https://medium.com/@bimsara.gunarathna/write-unit-tests-for-data-annotation-validator-attributes-in-net-ba9546e95f
+    [Fact]
+    public void CheepTextCannotBeEmpty()
+    {
+        //Arrange
+        Cheep cheep = new Cheep {
+            CheepId = 1,
+            Author = new Author { AuthorId = 1, Name = "Victor", Email = "vmem@itu.dk"},
+            AuthorId = 1,
+            Text = "", 
+            TimeStamp = DateTime.Now 
+        };
+
+        ValidationContext validationContext = new ValidationContext(cheep);
+        List<ValidationResult> validationResults = new List<ValidationResult>();
+
+        // Act
+        bool empty = Validator.TryValidateObject(cheep, validationContext, validationResults, true);
+
+        // Assert
+        Assert.False(empty);
+    }
+
+    [Fact]
+    public void CheepTextCannotBeLongerThan160Characters()
+    {
+        //Arrange
+        Cheep cheep = new Cheep {
+            CheepId = 1,
+            Author = new Author { AuthorId = 1, Name = "Victor", Email = "vmem@itu.dk"},
+            AuthorId = 1,
+            Text = new('a', 161),
+            TimeStamp = DateTime.Now
+        };
+
+        ValidationContext validationContext = new ValidationContext(cheep);
+        List<ValidationResult> validationResults = new List<ValidationResult>();
+
+        // Act
+        bool tooLong = Validator.TryValidateObject(cheep, validationContext, validationResults, true);
+
+        // Assert
+        Assert.False(tooLong);
     }
 }
