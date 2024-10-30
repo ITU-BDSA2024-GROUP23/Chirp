42ecce91ba013a0702d644a8be17914ec7c6bc8b
vmem <vmem@itu.dk>
2024-10-30 17:59:04 +0100

    migrations

--- src/Chirp.Infrastructure/Data/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/Data/ChirpDBContext.cs
@@ -1,6 +1,9 @@
+using Chirp.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-public class ChirpDBContext : DbContext
+public class ChirpDBContext : IdentityDbContext<User>
 {
     public DbSet<Cheep> Cheeps { get; set; }
     public DbSet<Author> Authors { get; set; }
