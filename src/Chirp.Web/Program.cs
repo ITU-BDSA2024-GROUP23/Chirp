7d68e57403cf2954448588bd61061566220c1b23
vmem <vmem@itu.dk>
2024-10-31 14:28:46 +0100

    progress

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -2,6 +2,7 @@ using Chirp.Infrastructure.Data;
 
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 public class Program
 {
