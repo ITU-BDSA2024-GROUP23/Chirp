27e3038e07bf3283ebfa576cc762bafe9d339097
vmem <vmem@itu.dk>
2024-10-31 19:34:59 +0100

    Update Author.cs
    Co-Authored-By: 21Philip <72072928+21Philip@users.noreply.github.com>
    Co-Authored-By: pete537d <24959520+pete537d@users.noreply.github.com>
    Co-Authored-By: Axel <118187716+Oksetorben@users.noreply.github.com>

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -2,7 +2,8 @@ using Chirp.Infrastructure.Data;
 
 public class Author
 {
-    public User AuthorId { get; set; }
+    public int AuthorId { get; set; }
+    public User UserId { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
     public List<Cheep> Cheeps { get; set; } = new List<Cheep>();
