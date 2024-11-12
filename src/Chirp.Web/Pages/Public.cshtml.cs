0d9f95b72677aa3bdf9cbce74c4f94cb4d9c0a5a
vmem <vmem@itu.dk>
2024-11-12 12:58:51 +0100

    safer cheepbox implemented in public

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,3 +1,6 @@
+using Chirp.Web.Pages.Shared.Models;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,12 +9,15 @@ namespace Chirp.Web.Pages;
 public class PublicModel : PageModel
 {
     private readonly ICheepRepository _repository;
-    public List<CheepDTO> Cheeps { get; set; }
+    public List<CheepDTO> Cheeps { get; set; } = new();
+    [BindProperty]
+    public CheepBoxModel CheepBox { get; set; } = new();
+    private readonly SignInManager<User>  _signInManager;
 
-    public PublicModel(ICheepRepository repository)
+    public PublicModel(ICheepRepository repository, SignInManager<User> signInManager)
     {
         _repository = repository;
-        Cheeps = new();
+        _signInManager = signInManager;
     }
 
     public ActionResult OnGet([FromQuery] int page = 1)
@@ -21,15 +27,21 @@ public class PublicModel : PageModel
         return Page();
     }
 
-    public IActionResult OnPost(string cheep)
+    public IActionResult OnPost()
     {
-        string? author = User.Identity?.Name;
-        if (string.IsNullOrEmpty(author))
+        User ?user = _signInManager.UserManager.GetUserAsync(User).Result;
+        if (user == null)
         {
             TempData["alert-error"] = "You must be logged in to post a cheep!";
             return RedirectToPage("Public");
         }
-        _repository.CreateCheep(author, cheep);
+        if (!ModelState.IsValid)
+        {
+            string errors = string.Join(" ", ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage)); // got this from https://stackoverflow.com/a/4934712
+            TempData["alert-error"] = errors;
+            return RedirectToPage("Public");
+        }
+        _repository.CreateCheep(user, CheepBox.Message ?? throw new InvalidOperationException("Cheep message is null!")); // we should never get to the exception
         TempData["alert-success"] = "Cheep posted successfully!";
         return RedirectToPage("Public");
     }
