34601b8bd1f207ba25cc4cc80de4d9f61d3c2bc2
vmem <vmem@itu.dk>
2024-11-15 12:53:43 +0100

    update to dbcontext

--- src/Chirp.Infrastructure/Data/ChirpDBContext.cs
+++ src/Chirp.Infrastructure/Data/ChirpDBContext.cs
@@ -4,12 +4,32 @@ using Microsoft.EntityFrameworkCore;
 public class ChirpDBContext : IdentityDbContext<User>
 {
     public DbSet<Cheep> Cheeps { get; set; }
+    public DbSet<Follower> Followers { get; set; }
 
     public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
     {
-        if (Database.EnsureCreated())
+        /*if (Database.EnsureCreated())
         {
-            //DbInitializer.SeedDatabase(this);
+            DbInitializer.SeedDatabase(this);
         }
+        */
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Follower>()
+            .HasKey(f => new { f.FollowerId, f.FolloweeId });
+
+        modelBuilder.Entity<Follower>()
+            .HasOne(f => f.FollowerUser)
+            .WithMany(u => u.Following)
+            .HasForeignKey(f => f.FollowerId);
+
+        modelBuilder.Entity<Follower>()
+            .HasOne(f => f.FolloweeUser)
+            .WithMany(u => u.Followers)
+            .HasForeignKey(f => f.FolloweeId);
     }
 }
