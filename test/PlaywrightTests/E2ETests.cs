1aa07e73d14ec9fb1d140f97a7919cb568f158b8
vmem <vmem@itu.dk>
2024-11-18 14:52:56 +0100

    more e2e tests

--- test/PlaywrightTests/E2ETests.cs
+++ test/PlaywrightTests/E2ETests.cs
@@ -15,12 +15,13 @@ public class E2ETests : PageTest
     public async Task Init()
     {
         _factory = new PlaywrightWebApplicationFactory<Program>();
-        _factory.CreateClient();
+        await Task.Run(() => _factory.CreateClient());
+
     }
 
 
     [Test]
-    public async Task Test_Layout()
+    public async Task TestLayoutUI()
     {
         await Page.GotoAsync("http://localhost:5273/");
         await Expect(Page.GetByRole(AriaRole.Heading)).ToContainTextAsync("Public Timeline");
@@ -32,7 +33,7 @@ public class E2ETests : PageTest
     }
 
     [Test]
-    public async Task Test_Register()
+    public async Task TestRegisterUI()
     {
         await Page.GotoAsync("http://localhost:5273/Identity/Account/Register");
         await Expect(Page.Locator("h2")).ToContainTextAsync("Create a new account");
@@ -43,7 +44,55 @@ public class E2ETests : PageTest
         await Expect(Page.Locator("#registerSubmit")).ToContainTextAsync("Register");
         await Expect(Page.Locator("h3")).ToContainTextAsync("Use another service to register");
         await Expect(Page.Locator("button[name=\"provider\"]")).ToContainTextAsync("Register with GitHub");
+    }
 
+    [Test]
+    public async Task TestCanRegister()
+    {
+        await InitTestUser();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Public Timeline" })).ToBeVisibleAsync();
+        await Expect(Page.GetByText("Whats on your mind ropf? Cheep 160 characters max")).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "logout [ropf]" })).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task TestCanLogin()
+    {
+        await InitTestUser();
+        await Page.GotoAsync("http://localhost:5273/Identity/Account/Login");
+        await Page.GetByPlaceholder("Email").FillAsync("ropf@itu.dk");
+        await Page.GetByPlaceholder("Email").PressAsync("Tab");
+        await Page.GetByPlaceholder("Password").FillAsync("LetM31n!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in", Exact = true }).ClickAsync();
+        await Expect(Page.GetByText("Whats on your mind ropf? Cheep 160 characters max")).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "logout [ropf]" })).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task TestCanLogout()
+    {
+        await InitTestUser();
+        await Page.GotoAsync("http://localhost:5273/Identity/Account/Logout");
+        await Expect(Page.GetByRole(AriaRole.Heading)).ToContainTextAsync("Log out of Chirp");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Click here to log out" }).ClickAsync();
+        await Expect(Page.GetByText("You have been logged out.")).ToBeVisibleAsync();
+    }
+
+    // This method is used to create a test user for the tests. - moved to a seperate method because an authed account is needed for multiple tests
+    // NOTE: If the registrations fails, all tests dependent on this method will fail.
+    private async Task InitTestUser()
+    {
+        await Page.GotoAsync("http://localhost:5273/Identity/Account/Register");
+        await Page.GetByPlaceholder("Username").FillAsync("ropf");
+        await Page.GetByPlaceholder("Username").PressAsync("Tab");
+        await Page.GetByPlaceholder("Email").FillAsync("ropf@itu.dk");
+        await Page.GetByPlaceholder("Email").PressAsync("Tab");
+        await Page.GetByPlaceholder("Password", new() { Exact = true }).FillAsync("LetM31n!");
+        await Page.GetByPlaceholder("Password", new() { Exact = true }).PressAsync("Tab");
+        await Page.GetByPlaceholder("Confirm Password").FillAsync("LetM31n!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register", Exact = true }).ClickAsync();
     }
 
     [TearDown]
